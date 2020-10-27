fb5631b393a220fd445d9bdb83a9bc404fbe8d1f
tal <tal.yaakov@campus.technion.ac.il>
2020-10-27 22:17:01 +0200

    debugging

--- CAT/CAT-Customized-Adversarial-Training-for-Improved-Robustness_suppl/code/main_adv_cus.py
+++ CAT/CAT-Customized-Adversarial-Training-for-Improved-Robustness_suppl/code/main_adv_cus.py
@@ -100,7 +100,7 @@ if opt.data == 'cifar10':
     ])
     trainset = torchvision.datasets.CIFAR10(root=opt.root, train=True, download=True, transform=transform_train)
     if opt.train_sampler:
-        trainloader = torch.utils.data.DataLoader(trainset, batch_size=10,  # TODO: change here to 128
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=15,  # TODO: change here to 128
                                                   shuffle=False, num_workers=2, pin_memory=True,
                                                   sampler=FixedRandomSampler(trainset))
     else:
@@ -110,7 +110,7 @@ if opt.data == 'cifar10':
     # trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
     # statloader = torch.utils.data.DataLoader(trainset, batch_size=500, shuffle=False, num_workers=2)
     testset = torchvision.datasets.CIFAR10(root=opt.root, train=False, download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=10, shuffle=False, num_workers=2)     # TODO: change here back to 100
+    testloader = torch.utils.data.DataLoader(testset, batch_size=15, shuffle=False, num_workers=2)     # TODO: change here back to 100
 elif opt.data == 'tiny_imagenet':
     nclass = 200
     img_width = 64
@@ -297,7 +297,7 @@ def train_soadp(epoch, perm, eps, cw=False, mixup_in_epoch=0, hidden_in_epoch=0,
     print('Epoch: %d' % epoch)
     net.train()
     train_loss = 0
-    batch_size = 10 # TODO: change here back to 128
+    batch_size = 15 # TODO: change here back to 128
     # the indices of the samples for producing adversarial examples
 
     hidden_index = list(random.sample(range(len(trainloader)), hidden_in_epoch))
